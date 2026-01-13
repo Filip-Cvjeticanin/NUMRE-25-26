81650bc3b8ab62582080f3214c2c5cbfd643a6b3
Filip-Cvjeticanin <filip.cvjeticanin@fer.unizg.hr>
2026-01-13 21:51:34 +0100

    count_examples() method in example_extractor.py

--- PyModuli/example_extractor.py
+++ PyModuli/example_extractor.py
@@ -24,12 +24,34 @@ def extract_examples(file_path):
                 yield example
 
 
+def count_examples(file_path):
+    count = 0
+    with open(file_path, newline="", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        header = next(reader)  # skip header
+
+        for row_idx, row in enumerate(reader):
+            if len(row) != 4:
+                continue
+
+            article, abstract, *_ = row
+            if article.strip() and abstract.strip():
+                count += 1
+
+    return count
+
+
+
 
 if __name__ == "__main__":
     # Example usage:
     file_path = "C:/Docs/01 - Filip/02 - FER/070 - G4S1/03-NM/Projekt/SkupPodataka-last.csv"
+
+    total = count_examples(file_path)
+    print("Total number of examples:", total)
+
     for i, example in enumerate(extract_examples(file_path)):
         print(example)
-        input()
+        input("input anything for next example:")
 
 
